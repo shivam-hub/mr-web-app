e772d0708fa6092ead0f1efa123be8ae6d797922
Ruuuchii <86821633+Ruuuchii@users.noreply.github.com>
2024-02-15 00:16:08 +0530

    login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,22 +7,28 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
 
 function Login() {
     return (
         <div className="h-screen flex justify-center items-center">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Login</CardTitle>
-                    <CardDescription>Login to continue...</CardDescription>
+            <Card className="w-80 mx-auto md:w-96 lg:w-104 max-h-96 p-8 bg-white rounded shadow-md">
+                <CardHeader className="flex justify-center items-center">
+                    <CardTitle className="font-bold text-2xl">Login</CardTitle>
                 </CardHeader>
-                <CardContent>
+                <CardContent >
                     <form action="">
+                    <Input type="text" placeholder="Username" className="my-5 "/>
+                    <Input type="password" placeholder="Password" className="my-2 " />
 
                     </form>
                 </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
+                <CardFooter className="flex justify-center ">
+                <Button>Log in</Button>
+ 
                 </CardFooter>
             </Card>
         </div>
