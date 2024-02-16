c75ce56ce19df8b63c1dc528bbe7b1fdc87b69c5
Shivam Pandey <shivampandey4194@gmail.com>
2024-02-17 00:02:32 +0530

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//     const path = req.nextUrl.pathname;
-//     const token = req.cookies.get('token')?.value || '';
-//     const isPublicPath = path === '/login';
-//     const url = req.nextUrl.clone()
-
-//     if(isPublicPath){
-//         if (token !== '') {
-//             url.pathname = '/user-workbench'
-//         }
-//         else{
-//             url.pathname = '/login'
-//         }
-//         return NextResponse.redirect(url);
-//     }
-//     if (token === '') {
-//         url.pathname = '/login'
-//         return NextResponse.redirect(url);
-//     }
-
-//     return NextResponse.next();
-// }
-
-// import { NextRequest, NextResponse } from "next/server";
-
-// import { NextRequest, NextResponse } from "next/server";
-
 export function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
     const token = req.cookies.get('token')?.value || '';
